659db605d09b05578056e9f0591316b40be22861
ID24-CMR <idrice.tsafouet@yahoo.com>
2024-11-21 08:10:52 +0100

    update docs folder #3

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './core/components/home/home.component';
+import { CoreModule } from './core/core.module';
 
 export const routes: Routes = [
-	{path: '', redirectTo:'home', pathMatch: 'full'},
+	{path: '', redirectTo: '/home',pathMatch: 'full'},
 	{path: 'home', component: HomeComponent}];
+	
